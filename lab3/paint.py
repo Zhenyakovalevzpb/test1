59973c8a547e20ab10e4a9f019ff92fb6949ef33
Zhenyakovalevzpb <zhenyakova1466@gmail.com>
2020-03-27 14:46:06 +0300

    ignore diff directories

---
+++ lab3/paint.py
@@ -0,0 +1,21 @@
+from graph import *
+penColor(0,0,0)
+penSize(1)
+brushColor("yellow")
+circle(300, 300, 100)
+
+brushColor("red")
+circle(250, 265, 25)
+circle(350, 265, 17)
+
+
+brushColor("black")
+rectangle(235, 330, 365, 350)
+
+polygon([(195,205), (190,215),
+         (295,255), (300,245)])
+polygon([(410,210), (405,200),
+         (320,250), (325,260)])
+circle(250, 265, 9)
+circle(350, 265, 9)
+run()
